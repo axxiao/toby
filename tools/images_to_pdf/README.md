56c1b4c1c236872697d616d7da65361ad5134806
axxiao <xiaocrm@gmail.com>
2020-12-16 22:40:24 +1100

    Reorg images to pdf

---
+++ tools/images_to_pdf/README.md
@@ -0,0 +1,4 @@
+#Toby
+Toby is Alex's personal project.
+
+Alex Xiao
---
+++ tools/images_to_pdf/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from images_to_pdf import create_pdf_from_images
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="images_to_pdf",
+    version="1.0.0",
+    author="Alex Xiao",
+    author_email="alex.xiao@gmail.com",
+    description="A tool to combine images into PDF file(s)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['images_to_pdf=images_to_pdf.create_pdf_from_images:main']
+    },
+    python_requires='>=3.6',
+)
